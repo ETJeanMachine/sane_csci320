040670d07c7dcda31e2f5a9f1ca3a4a83f9b577c
ericjean873@gmail.com <etj2206@rit.edu>
2020-10-14 18:16:22 -0400

    Setting up the postgres connection

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,19 @@
+import java.sql.*;
+
+/**
+ * The JavaFX GUI application that interacts with our database.
+ */
 public class Application {
+
+    // TODO
+    public static void main(String[] args) {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://reddwarf.cs.rit.edu:5432/p320_14", "p320_14", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": " + e.getMessage());
+        }
+    }
 }
