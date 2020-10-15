70a615863a50365013b3f6c5a42368aadcc35009
ericjean873@gmail.com <etj2206@rit.edu>
2020-10-15 03:48:24 -0400

    Added more password functionality on the GUI

--- src/ui/MainGUI.java
+++ src/ui/MainGUI.java
@@ -8,43 +8,40 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import model.Database;
 import model.Song;
 
 public class MainGUI extends Application {
 
-    private FlowPane pane;
+    private Pane mainPane = new Pane();
+    private FlowPane loginPane;
     private Database db;
 
     public void init() {
-        pane = new FlowPane();
+        // Sets up the login screen for the database.
+        loginPane = new FlowPane();
         PasswordField passField = new PasswordField();
         passField.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 try {
                     db = new Database(passField.getText());
-                    pane.getChildren().remove(0);
-                    pane.getChildren().remove(1);
+                    mainPane.getChildren().remove(0);
                 } catch (Exception e) {
-                    pane.getChildren().add(new Label("Incorrect Login! Try again."));
+                    passField.setText("");
+                    loginPane.getChildren().add(new Label("Incorrect Login! Try again."));
                 }
             }
         });
-        pane.getChildren().add(passField);
+        loginPane.getChildren().add(passField);
+        mainPane.getChildren().add(loginPane);
     }
 
     @Override
     public void start(Stage stage) {
-        //Database db = new Database(PASSWORD);
-        /*
-        for(Song song : db.getDatabaseSongs()) {
-            Label l = new Label(song.toString());
-            pane.getChildren().add(l);
-        }
-        */
-        stage.setScene(new Scene(pane));
+        stage.setScene(new Scene(mainPane));
         stage.show();
     }
 
